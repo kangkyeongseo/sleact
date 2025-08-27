a444774bb10afbabf27430471c9a4a524be385f8
kangkyeongseo <kks_big@naver.com>
2025-08-27 16:03:12 +0900

    Feat: Login Page

--- front/layouts/App.tsx
+++ front/layouts/App.tsx
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 
 const Login = lazy(() => import('@pages/Login'));
-const SingUp = lazy(() => import('@pages/SingUp'));
+const SingUp = lazy(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
--- front/pages/Login/index.tsx
+++ front/pages/Login/index.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import useSWR from 'swr';
+import axios from 'axios';
+import useInput from '@hooks/useInput';
+import fetcher from '@utils/fetcher';
+import { Link } from 'react-router';
+import { Button, Error, Form, Header, Input, Label, LinkContainer } from '@pages/SignUp/styles';
 
 const Login = () => {
-  return <div>Login</div>;
+  const { data, error, isLoading } = useSWR('http://localhost:3095/api/users', fetcher);
+  const [logInError, setLogInError] = useState(false);
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+
+  const onSubmit = useCallback(
+    (event: React.ChangeEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setLogInError(false);
+      axios
+        .post('http://localhost:3095/api/users/login', { email, password }, { withCredentials: true })
+        .then(() => {})
+        .catch(() => {})
+        .finally(() => {});
+    },
+    [email, password],
+  );
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+          {logInError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">로그인</Button>
+      </Form>
+      <LinkContainer>
+        아직 회원이 아니신가요?&nbsp;
+        <Link to="/signup">회원가입 하러가기</Link>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default Login;
