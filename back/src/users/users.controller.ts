a8d0c73d3372c54cb813d436964fbd125805218f
kangkyeongseo <kks_big@naver.com>
2025-09-01 10:52:16 +0900

    Feat: Decorator and Interceptor

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,9 +1,12 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseInterceptors } from '@nestjs/common';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserDto } from 'src/common/dto/user.dto';
+import { User } from 'src/common/decorator/user.decorator';
+import { UndefinedToNullInterceptor } from 'src/common/interceptors/undefinedToNull.interceptor';
 
+@UseInterceptors(UndefinedToNullInterceptor)
 @ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
@@ -12,8 +15,8 @@ export class UsersController {
   @ApiResponse({ status: 200, description: '성공', type: UserDto })
   @ApiOperation({ summary: '내 정보 조회' })
   @Get()
-  getUsers(@Req() req) {
-    return req.users;
+  getUsers(@User() user) {
+    return user;
   }
 
   @ApiOperation({ summary: '회원가입' })
@@ -26,7 +29,9 @@ export class UsersController {
   @ApiResponse({ status: 500, description: '서버 에러' })
   @ApiOperation({ summary: '로그인' })
   @Post('login')
-  logIn() {}
+  logIn(@User() user) {
+    return user;
+  }
 
   @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
